670a23437ac0d6470fa47a2f1750f66a81c6296d
William Obando <wocar01@gmail.com>
2015-03-20 23:20:38 +0100

    Commit missing file

---
+++ App.DLL/Data/mdf_incorrect_mdfstream.cs
@@ -0,0 +1,14 @@
+﻿using App.DLL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DLL.Data
+{
+    public class mdf_stream_incorrect : mdf_stream 
+    {
+
+    }
+}
