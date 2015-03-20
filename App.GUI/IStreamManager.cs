5dd5f77807620544eaa7b9736b0018a023f0025e
longstone <git@longstone.ch>
2015-03-20 21:11:53 +0100

    aded IStreamManager

---
+++ App.GUI/IStreamManager.cs
@@ -0,0 +1,13 @@
+﻿using GUI.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App.GUI.StreamManager
+{
+    class IStreamManager
+    {
+        public void process(mdf_stream entry);
+    }
+}
