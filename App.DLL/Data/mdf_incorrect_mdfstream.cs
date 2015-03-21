a1f2832cb0c88acc74b393cd53f495b5734179d7
William Obando <wocar01@gmail.com>
2015-03-21 10:10:36 +0100

    Added incorrect streams table

--- App.DLL/Data/mdf_incorrect_mdfstream.cs
+++ App.DLL/Data/mdf_incorrect_mdfstream.cs
@@ -1,12 +1,14 @@
 ﻿using App.DLL.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace App.DLL.Data
 {
+    [Table("Incorrect_streams")]
     public class mdf_stream_incorrect : mdf_stream 
     {
 
